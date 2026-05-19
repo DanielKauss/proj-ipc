2f438e0bca381f44b18640c9f7d77ea2097a2d9b
RubénPuigLópez <puiglopezruben@gmail.com>
2026-05-19 16:09:43 +0200

    Pagina principal controller empezado

--- src/app/controllers/PaginaPrincipalController.java
+++ src/app/controllers/PaginaPrincipalController.java
@@ -1,8 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
-package resources.fxml;
+  package app.controllers;
 
 import app.Controller;
 import java.net.URL;
