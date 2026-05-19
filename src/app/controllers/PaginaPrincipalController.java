3e8e5254d2fd95eba09225c50c52853c358586e4
RubénPuigLópez <puiglopezruben@gmail.com>
2026-05-19 15:52:10 +0200

    Pagina principal fxml acabado

---
+++ src/app/controllers/PaginaPrincipalController.java
@@ -0,0 +1,68 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package resources.fxml;
+
+import app.Controller;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.image.ImageView;
+
+/**
+ * FXML Controller class
+ *
+ * @author Usuario
+ */
+public class PaginaPrincipalController extends Controller {
+
+    @FXML
+    private Button añadir;
+    @FXML
+    private Button mapas;
+    @FXML
+    private Button perfil;
+    @FXML
+    private Button historial;
+    @FXML
+    private Button cerrar;
+    @FXML
+    private ListView<?> lista;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+
+    @FXML
+    private void mapas(ActionEvent event) {
+         changeScene("MapaDemo");
+    }
+
+    @FXML
+    private void perfil(ActionEvent event) {
+    }
+
+    @FXML
+    private void historialSesiones(ActionEvent event) {
+    }
+
+    @FXML
+    private void cerrarSesion(ActionEvent event) {
+         changeScene("CerrarSesion");
+    }
+
+    @FXML
+    private void actividadNueva(ActionEvent event) {
+    }
+    
+}
