bd4142eb8c07794419598966e67d5ce3eb3e9a03
Ruben <puiglopezruben@gmail.com>
2026-05-24 11:50:03 +0200

    fuente del titulo ahora si

--- src/app/App.java
+++ src/app/App.java
@@ -11,6 +11,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import upv.ipc.sportlib.SportActivityApp;
 
@@ -19,19 +20,24 @@ import upv.ipc.sportlib.SportActivityApp;
  * @author daniel
  */
 public class App extends Application {
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         //System.out.println(SportActivityApp.getInstance().login("juan_23", "juan_23!"));
         //Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/PaginaPrincipal.fxml"));
-
+        Font.loadFont(
+                getClass().getResourceAsStream(
+                        "/fonts/MervaleScrpit-Regular.ttf"
+                ),
+                80
+        );
         Parent root = FXMLLoader.load(getClass().getResource("/resources/fxml/LandingPage.fxml"));
         stage.getIcons().add(new Image(getClass().getResourceAsStream("/resources/images/logo.png")));
         Scene scene = new Scene(root);
         stage.setTitle("Running La Safor");
         stage.setScene(scene);
         stage.show();
-        
+
         Controller.stage = stage;
     }
 
@@ -41,5 +47,5 @@ public class App extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    
+
 }
